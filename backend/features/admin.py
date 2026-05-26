fecc82c159601f5fffeaa8bd585b3a6a66c24eb9
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-05-26 15:48:24 +0530

    registering models for admin

--- backend/features/admin.py
+++ backend/features/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (Rooms, Bookings, weekly_events, food_menu, 
+                     special_events, membership_plans, Privilages, 
+                     Redemption_items, Orders, Offerorder, EventBooking, Redemption)
 
-# Register your models here.
+admin.site.register(Rooms)
+admin.site.register(Bookings)
+admin.site.register(weekly_events)
+admin.site.register(food_menu)
+admin.site.register(special_events)
+admin.site.register(membership_plans)
+admin.site.register(Privilages)
+admin.site.register(Redemption_items)
+admin.site.register(Orders)
+admin.site.register(Offerorder)
+admin.site.register(EventBooking)
+admin.site.register(Redemption)
