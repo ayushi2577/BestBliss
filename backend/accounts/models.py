94f38b75bcc9c1533594b1730b0531f42a493f34
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-05-26 15:44:32 +0530

    setting up superuser

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -46,6 +46,12 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
     
+    def has_perm(self, perm, obj=None):
+        return self.is_admin or self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_admin or self.is_superuser
+    
     def save(self, *args, **kwargs):
         #custom ID (e.g., BB-1001, BB-1002...) if it doesn't exist yet
         if not self.id:
