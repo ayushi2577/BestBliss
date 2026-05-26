3b8d5dddcc335cb28c1c7c5968d66e3ad1e823b2
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-05-26 15:30:19 +0530

    setting up superuser

--- backend/build.sh
+++ backend/build.sh
@@ -6,8 +6,8 @@ python manage.py migrate
 echo "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'Admin@1234')
+if not User.objects.filter(email='admin@example.com').exists():
+    User.objects.create_superuser(email='admin@example.com', password='Admin@1234')
     print('Superuser created')
 else:
     print('Superuser already exists')
