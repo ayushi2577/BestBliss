e73be67ab9fb306fe19459241a070d18986612ee
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-05-26 15:28:33 +0530

    setting up superuser

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin@1234')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+" | python manage.py shell
